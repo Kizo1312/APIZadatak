565621df6f9b8363fbd0aed9b4a64502735ca863
Toma Ilcic <toma.ilcic@gmail.com>
2023-10-02 18:02:29 +0200

    APITask

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+import "./App.css";
+import NasaImage from "./NasaImage";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NasaImage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
